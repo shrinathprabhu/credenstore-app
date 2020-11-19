4552921e30e49ea5c7523470f0d6a6e380076309
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-11-19 07:58:23 +0530

    Added logos and simple material app with blank scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Credenstore());
 
-class MyApp extends StatelessWidget {
+class Credenstore extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return new App();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class App extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _AppState createState() => _AppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AppState extends State<App> {
+  Icon themeIcon = Icon(Icons.brightness_4);
+  String themeMode = 'light';
+  ThemeMode _themeMode = ThemeMode.light;
 
-  void _incrementCounter() {
+  // custom functions
+  MaterialColor _createMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    Map swatch = <int, Color>{};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    strengths.forEach((strength) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    });
+    return MaterialColor(color.value, swatch);
+  }
+
+  void _launchPaypal() async {
+    const url = 'https://paypal.me/shrinathprabhu';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  void _toggleTheme() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (themeMode == 'light') {
+        themeMode = 'dark';
+        themeIcon = Icon(Icons.brightness_7);
+        _themeMode = ThemeMode.dark;
+      } else {
+        themeMode = 'light';
+        themeIcon = Icon(Icons.brightness_4);
+        _themeMode = ThemeMode.light;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Credenstore',
+      themeMode: _themeMode,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: _createMaterialColor(Color(0xFF35495E)),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: _createMaterialColor(Color(0xFF35495E)),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: Image(
+                image: NetworkImage(
+                    "https://credenstore.web.app/img/logo.e1bdf621.png"),
+                height: 25,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Text("Credenstore"),
+          ]),
+          actions: <Widget>[
+            Tooltip(
+              message: "Buy me a coffee",
+              child: IconButton(
+                icon: Icon(Icons.payment),
+                color: Colors.white,
+                onPressed:
+                    _launchPaypal, // This trailing comma makes auto-formatting nicer for build methods.
+              ),
+            ),
+            Tooltip(
+              message: "Change theme",
+              child: IconButton(
+                icon: themeIcon,
+                color: Colors.white,
+                onPressed:
+                    _toggleTheme, // This trailing comma makes auto-formatting nicer for build methods.
+              ),
             ),
           ],
         ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Welcome to credenstore',
+              ),
+            ],
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
